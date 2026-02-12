579525b33a44a6af33dd44d3d4e3dfd6d29d5251
Rafael Campos <minhaequipeads@gmail.com>
2026-02-12 20:00:07 -0300

    build: adiciona schema controle_obras, configuração JPA e carregamento de variáveis .env

---
+++ init-db/01-create-schema.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA IF NOT EXISTS controle_obras;
--- src/main/java/br/ce/sop/gestaoorcamento/GestaoOrcamentoApplication.java
+++ src/main/java/br/ce/sop/gestaoorcamento/GestaoOrcamentoApplication.java
@@ -1,5 +1,6 @@
 package br.ce.sop.gestaoorcamento;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestaoOrcamentoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(GestaoOrcamentoApplication.class, args);
 	}
-
 }
