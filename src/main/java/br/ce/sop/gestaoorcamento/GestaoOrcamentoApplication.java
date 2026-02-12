4d2d026197fc641d57890e484501f71ba70088a0
Rafael Campos <minhaequipeads@gmail.com>
2026-02-12 14:26:25 -0300

    build: initial structure from spring initializr

---
+++ src/main/java/br/ce/sop/gestaoorcamento/GestaoOrcamentoApplication.java
@@ -0,0 +1,13 @@
+package br.ce.sop.gestaoorcamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoOrcamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoOrcamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ce/sop/gestaoorcamento/GestaoOrcamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ce.sop.gestaoorcamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoOrcamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
